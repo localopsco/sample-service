33c140555df20c659dbd18096c205b9b47510067
Anand <108571311+localops-root@users.noreply.github.com>
2025-05-08 10:02:52 +0530

    Update internal.js

--- internal.js
+++ internal.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3001
 
 app.get('/', (req, res) => {
-	res.send('Internal service; Hello there')
+	res.send('Internal service here; Hello there')
 })
 
 app.listen(port, () => {
