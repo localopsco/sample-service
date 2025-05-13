ce1b68d6d8fcda332f906495975a334bb74759c6
localops-root <anand@localops.co>
2025-05-13 06:55:19 +0530

    fix log

--- web.js
+++ web.js
@@ -8,7 +8,7 @@ const serviceStartTime = new Date()
 
 app.get('/', (req, res) => {
 	console.log("Request received at", new Date().toISOString())
-	console.log("Processing at", new Date().toISOString())
+	console.log("Processing at time:", new Date().toISOString())
 	res.send(`Hello.. Env:${process.env.ENV} \n\nStarted at: ${formatRelative(serviceStartTime, new Date())}`)
 })
 
