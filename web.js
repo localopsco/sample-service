5bb393d37cc81c4fa9371cfc540dedcb1289425c
localops-root <anand@localops.co>
2025-08-16 15:55:55 +0530

    refine reponse html

--- web.js
+++ web.js
@@ -9,7 +9,7 @@ const serviceStartTime = new Date()
 app.get('/', (req, res) => {
 	console.log("Request received at", new Date().toISOString())
 	console.log("Processing request at", new Date().toISOString())
-	res.send(`<h2>Hello..Env:${process.env.ENV} \n\nStarted at: ${formatRelative(serviceStartTime, new Date())}</h2>`)
+	res.send(`<h2>Hello from Env:${process.env.ENV}.</h2> Started at: ${formatRelative(serviceStartTime, new Date())}`)
 })
 
 app.listen(port, () => {
