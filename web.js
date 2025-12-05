fa0632a648d155a1381b0d84e61dd55742835008
Anand <108571311+localops-root@users.noreply.github.com>
2025-12-05 21:22:01 +0530

    Update web.js

--- web.js
+++ web.js
@@ -8,7 +8,7 @@ const serviceStartTime = new Date()
 
 app.get('/', (req, res) => {
 	console.log("Request received at", new Date().toISOString())
-	console.log("Processing request at", new Date().toISOString())
+	console.log("Processing request", new Date().toISOString())
 	res.json({
 		message: "hello",
 		env: process.env.ENV,
