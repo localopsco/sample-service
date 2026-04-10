066bfd850adaba903935abae0857cb0a88b918f0
Sibiraj <134702220+opswiz@users.noreply.github.com>
2026-04-10 17:02:48 +0530

    Update greeting message in web response

--- web.js
+++ web.js
@@ -9,7 +9,7 @@ const serviceStartTime = new Date()
 app.get('/', (req, res) => {
 	console.log("Request received at", new Date().toISOString())
 	console.log("Processing request at", new Date().toISOString())
-	res.send(`<h2>Hello Env:${process.env.ENV}.</h2> Started at: ${formatRelative(serviceStartTime, new Date())}`)
+	res.send(`<h2>Hello Test web Env:${process.env.ENV}.</h2> Started at: ${formatRelative(serviceStartTime, new Date())}`)
 })
 
 app.listen(port, () => {
