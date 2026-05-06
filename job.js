c3b81836f869935a6dac647eaa79923a013115c8
Sibiraj <134702220+opswiz@users.noreply.github.com>
2026-05-06 14:09:38 +0530

    Fix log message for job end timestamp

--- job.js
+++ job.js
@@ -2,6 +2,6 @@ console.log("args:", process.argv)
 console.log("Job started:", new Date().toISOString())
 
 setTimeout(() => {
-	console.log("Job ended:", new Date().toISOString())
+	console.log("Job ended::", new Date().toISOString())
 }, 25000)
 
