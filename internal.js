f6c32436327d934fe13a1d1bde59a9f8f8928126
localops-root <anand@localops.co>
2026-05-13 14:09:37 +0530

    add metrics server to internal server

--- internal.js
+++ internal.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 3001
+const promClient = require('prom-client');
 
 app.get('/', (req, res) => {
 	res.send('Internal service here; Hello there')
@@ -11,3 +12,17 @@ app.listen(port, () => {
 	console.log("Internal service running on port:", port)
 })
 
+
+
+// Metrics server — separate port
+const metrics = express();
+
+promClient.collectDefaultMetrics();
+
+metrics.get('/metrics', async (req, res) => {
+  console.log("Metrics request received at", new Date().toISOString())
+  res.set('Content-Type', promClient.register.contentType);
+  res.end(await promClient.register.metrics());
+});
+
+metrics.listen(9090, () => console.log('Metrics on port 9090'));
